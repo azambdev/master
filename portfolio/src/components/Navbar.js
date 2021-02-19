712949441d0198ddf076a448104b803bd6ff8eb4
Ariel Zamb <azambdev1@gmail.com>
2021-02-19 01:56:59 -0300

    Navbar styling
    icon, color and behavior

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React from "react";
 import logotrans from "../mylogo.png";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
@@ -8,7 +11,7 @@ const Navbar = () => {
       
         <a className="navbar-brand" href="#"> <img src={logotrans} className="logo" alt="logo..."/></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+          <FontAwesomeIcon icon={faBars} style={{ color:"#fff" }}/>
         </button>
       
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
