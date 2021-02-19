d81e3bc770a3b67de354186416c041e6a76586fc
Ariel Zamb <azambdev1@gmail.com>
2021-02-19 02:50:26 -0300

    Header
    first header version with typed

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+              <div className="main-info">
+                   <h1>Web development and websites promotion </h1>  
+                   <Typed  
+                   className="typed-text" 
+                    strings={["Web design","Web development", "Databases","Cloud" ]}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    loop
+                    />  
+                    
+              </div> 
+        </div>
+    )
+}
+
+export default Header
