d598a3e360fabaef38cfdd99a47f238c17882cb7
Ariel Zamb <azambdev1@gmail.com>
2021-02-28 01:34:41 -0300

    Services
    Icons, format, styling

--- portfolio/src/components/Services.js
+++ portfolio/src/components/Services.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faFacebookF, faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faDesktop, faFileCode } from "@fortawesome/free-solid-svg-icons";
+
 
 const Services = () => {
     return (
 
         <div className="services">
 
-            <h1 className="py-5">
+            <h1 className="py-5">  My services </h1>
                 <div className="container">
                     <div className="row">
                         {/**/}
                         <div className="col-md-3 col-sm-6">
                             <div className="box">
+                           <div className="circle"><FontAwesomeIcon className="icon" icon={faDesktop} size="2x"/>   </div>
                                 <h3>Web design</h3>
                                 <p>I approach each project individually and always focus on the results.</p>
                             </div>
@@ -20,6 +25,7 @@ const Services = () => {
                         {/**/}
                         <div className="col-md-3 col-sm-6">
                             <div className="box">
+                            <div className="circle"><FontAwesomeIcon className="icon" icon={faFileCode} size="2x"/>   </div>
                                 <h3>Web development</h3>
                                 <p>I approach each project individually and always focus on the results.</p>
                             </div>
@@ -29,6 +35,7 @@ const Services = () => {
                         {/**/}
                         <div className="col-md-3 col-sm-6">
                             <div className="box">
+                            <div className="circle"><FontAwesomeIcon className="icon" icon={faFacebookF} size="2x"/>   </div>
                                 <h3>Facebook Ads</h3>
                                 <p>I approach each project individually and always focus on the results.</p>
                             </div>
@@ -38,6 +45,7 @@ const Services = () => {
                         {/**/}
                         <div className="col-md-3 col-sm-6">
                             <div className="box">
+                            <div className="circle"><FontAwesomeIcon className="icon" icon={faGoogle} size="2x"/>   </div>
                                 <h3>Google Ads</h3>
                                 <p>I approach each project individually and always focus on the results.</p>
                             </div>
@@ -56,7 +64,7 @@ const Services = () => {
                 </div>
 
 
-            </h1>
+           
         </div>
 
 
