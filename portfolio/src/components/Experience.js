5fd8cf37c6335f48bd1b8e0a4306d83120aba4f2
Ariel Zamb <azambdev1@gmail.com>
2021-02-28 01:56:29 -0300

    first commit
    Experience, first commit. Component created

---
+++ portfolio/src/components/Experience.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Experience = () => {
+    return (
+        <div className="experience">
+            <div className="d-flex justify-content-center my-5">
+
+                <h1>Experience</h1>
+            </div>
+            <div className="container experience-wrapper">
+
+            </div>
+        </div>
+
+    )
+}
+
+export default Experience
